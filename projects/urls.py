e9d8cb66f1f62a54a93f38de7c6e4ed7e0fbeff5
matheeshaw25 <matheeshawijerathna@gmai.com>
2024-04-16 22:28:34 +0530

    Minor changes

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ urlpatterns=[
     path('',views.projects, name="projects"), #url route, execute function name
     path('project/<str:pk>/',views.project, name="project"),#project/1, views.py.project function, function name
     path('create-project/',views.createProject,name= "create-project"),
-    path('update-project/<str:pk>/',views.updateProject, name="update-project"),
+    path('update-project/<str:pk>/',views.updateProject, name="update-project"), # name is the key
 
     path('delete-project/<str:pk>/',views.deleteProject, name="delete-project"),
 ]
